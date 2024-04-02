423c772d86ec6127ab9a84cb8994bb760eb27cfe
root <root@devops-stack>
2024-04-02 16:33:00 +0000

    init jobs

---
+++ jobs/playwright/play-wright-job.groovy
@@ -0,0 +1,10 @@
+job('play-wright-job') {
+  scm {
+    git {
+      remote {
+        url('https://github.com/mustaphaibnel/ssl-automate-manager-python')
+      }
+      branch('main')
+    }
+  }
+}
